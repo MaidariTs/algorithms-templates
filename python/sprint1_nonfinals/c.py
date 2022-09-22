ac7c29787f2a924c372f574ad37afc2ebd7e9a88
MaidariTs <tmaidari@mail.ru>
2022-09-23 02:05:29 +0300

    asdasd ;

--- python/sprint1_nonfinals/c.py
+++ python/sprint1_nonfinals/c.py
@@ -1,8 +1,67 @@
 from typing import List, Tuple
 
+
 def get_neighbours(matrix: List[List[int]], row: int, col: int) -> List[int]:
-    # Здесь реализация вашего решения
-    pass
+    len_matrix_row = len(matrix) - 1
+    len_matrix_col = len(matrix[0]) - 1
+    x = matrix[row][col]
+    list = []
+    # Добавить условие на кол-во столбцов и строк
+
+    if x == matrix[0][0]:
+        list.append(matrix[row][col+1])
+        list.append(matrix[row+1][col])
+        return sorted(list)
+
+    elif x == matrix[row][0]:
+        list.append(matrix[row][col+1])
+        list.append(matrix[row-1][col])
+        list.append(matrix[row+1][col])
+        return sorted(list)
+
+    elif x == matrix[0][len_matrix_col]:
+        list.append(matrix[row][col-1])
+        list.append(matrix[row+1][col])
+        return sorted(list)
+
+    elif x == matrix[len_matrix_row][0]:
+        list.append(matrix[row][col+1])
+        list.append(matrix[row-1][col])
+        return sorted(list)
+
+    elif x == matrix[len_matrix_row][len_matrix_col]:
+        list.append(matrix[row][col-1])
+        list.append(matrix[row-1][col])
+        return sorted(list)
+
+    elif x == matrix[row][len_matrix_col] and (
+         x != matrix[0][len_matrix_col] or
+         x != matrix[len_matrix_row][len_matrix_col]):
+        list.append(matrix[row][col-1])
+        list.append(matrix[row-1][col])
+        list.append(matrix[row+1][col])
+        return sorted(list)
+
+    elif x == matrix[0][col] and (
+         x != matrix[0][0] or x != matrix[0][len_matrix_col]):
+        list.append(matrix[row][col-1])
+        list.append(matrix[row][col+1])
+        list.append(matrix[row+1][col])
+        return sorted(list)
+
+    elif x == matrix[len_matrix_row][col]:
+        list.append(matrix[row][col-1])
+        list.append(matrix[row][col+1])
+        list.append(matrix[row-1][col])
+        return sorted(list)
+
+    else:
+        list.append(matrix[row][col-1])
+        list.append(matrix[row][col+1])
+        list.append(matrix[row-1][col])
+        list.append(matrix[row+1][col])
+        return sorted(list)
+
 
 def read_input() -> Tuple[List[List[int]], int, int]:
     n = int(input())
@@ -13,6 +72,7 @@ def read_input() -> Tuple[List[List[int]], int, int]:
     row = int(input())
     col = int(input())
     return matrix, row, col
+
 
 matrix, row, col = read_input()
 print(" ".join(map(str, get_neighbours(matrix, row, col))))
