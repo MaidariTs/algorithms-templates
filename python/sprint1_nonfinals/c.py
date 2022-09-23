54e2917ed4f15fe353ec1a5cc960ce7ec69357da
MaidariTs <tmaidari@mail.ru>
2022-09-23 13:09:46 +0300

    sadsda

--- python/sprint1_nonfinals/c.py
+++ python/sprint1_nonfinals/c.py
@@ -2,65 +2,17 @@ from typing import List, Tuple
 
 
 def get_neighbours(matrix: List[List[int]], row: int, col: int) -> List[int]:
-    len_matrix_row = len(matrix) - 1
-    len_matrix_col = len(matrix[0]) - 1
-    x = matrix[row][col]
+    len_matrix_col = len(matrix[0])
     list = []
-    # Добавить условие на кол-во столбцов и строк
-
-    if x == matrix[0][0]:
-        list.append(matrix[row][col+1])
-        list.append(matrix[row+1][col])
-        return sorted(list)
-
-    elif x == matrix[row][0]:
-        list.append(matrix[row][col+1])
-        list.append(matrix[row-1][col])
-        list.append(matrix[row+1][col])
-        return sorted(list)
-
-    elif x == matrix[0][len_matrix_col]:
-        list.append(matrix[row][col-1])
-        list.append(matrix[row+1][col])
-        return sorted(list)
-
-    elif x == matrix[len_matrix_row][0]:
-        list.append(matrix[row][col+1])
-        list.append(matrix[row-1][col])
-        return sorted(list)
-
-    elif x == matrix[len_matrix_row][len_matrix_col]:
-        list.append(matrix[row][col-1])
-        list.append(matrix[row-1][col])
-        return sorted(list)
-
-    elif x == matrix[row][len_matrix_col] and (
-         x != matrix[0][len_matrix_col] or
-         x != matrix[len_matrix_row][len_matrix_col]):
-        list.append(matrix[row][col-1])
-        list.append(matrix[row-1][col])
-        list.append(matrix[row+1][col])
-        return sorted(list)
-
-    elif x == matrix[0][col] and (
-         x != matrix[0][0] or x != matrix[0][len_matrix_col]):
-        list.append(matrix[row][col-1])
-        list.append(matrix[row][col+1])
-        list.append(matrix[row+1][col])
-        return sorted(list)
-
-    elif x == matrix[len_matrix_row][col]:
-        list.append(matrix[row][col-1])
-        list.append(matrix[row][col+1])
-        list.append(matrix[row-1][col])
-        return sorted(list)
-
-    else:
-        list.append(matrix[row][col-1])
-        list.append(matrix[row][col+1])
-        list.append(matrix[row-1][col])
-        list.append(matrix[row+1][col])
-        return sorted(list)
+    if col + 1 < len_matrix_col:
+        list.append(matrix[row][col + 1])
+    if col - 1 >= 0:
+        list.append(matrix[row][col - 1])
+    if row + 1 < len(matrix):
+        list.append(matrix[row + 1][col])
+    if row - 1 >= 0:
+        list.append(matrix[row - 1][col])
+    return sorted(list)
 
 
 def read_input() -> Tuple[List[List[int]], int, int]:
