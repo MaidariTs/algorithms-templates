24f8ef6b890cee459f4ed95f4d805559ee45f6c3
MaidariTs <tmaidari@mail.ru>
2022-09-23 18:12:40 +0300

    sprint1 g added

--- python/sprint1_nonfinals/g.py
+++ python/sprint1_nonfinals/g.py
@@ -1,8 +1,29 @@
 def to_binary(number: int) -> str:
-    # Здесь реализация вашего решения
-    pass
+    """
+    Деление
+    a = a / b
+    a /= b
+
+    Целая часть
+    a = a // b
+    a //=b
+
+    Остаток
+    a = a % b
+    a %= b
+    """
+
+    b = ''
+
+    while number > 0:
+        b = str(number % 2) + b
+        number = number // 2
+
+    return b
+
 
 def read_input() -> int:
     return int(input().strip())
+
 
 print(to_binary(read_input()))
