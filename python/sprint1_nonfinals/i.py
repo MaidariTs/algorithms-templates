db1808fa9d61f1ce243861a27445c5e4739c8796
Maidari Tsydenov <tmaidari@mail.ru>
2022-09-25 05:00:04 +0300

    task J added

--- python/sprint1_nonfinals/i.py
+++ python/sprint1_nonfinals/i.py
@@ -1,5 +1,18 @@
 def is_power_of_four(number: int) -> bool:
-    # Здесь реализация вашего решения
-    pass
+    if number == 1:
+        return True
+    list = []
+    n = 1
+    a = 1
+    b = 4
+    while n < 10000:
+        n = b**a
+        a += 1
+        list.append(n)
+    aw = list
+    if number in aw:
+        return True
+    return False
+
 
 print(is_power_of_four(int(input())))
