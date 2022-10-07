c8ce17229b17de806268bbb2300b11b967d4d3d8
Maidari Tsydenov <tmaidari@mail.ru>
2022-10-07 14:31:40 +0300

    sprint12 H added

--- python/sprint12/H.py
+++ python/sprint12/H.py
@@ -1,11 +1,41 @@
+from typing import List
+
+
+class Stack():
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+
+def is_correct_bracket_seq(seq: List[str]) -> bool:
+    brackets = {'(': ')', '{': '}', '[': ']'}
+    stack = Stack()
+    for i in seq:
+        if i in brackets.keys():
+            stack.push(i)
+        elif len(stack.items) != 0 and brackets[stack.peek()] == i:
+            stack.pop()
+        else:
+            stack.push(i)
+            break
+
+    if len(stack.items) == 0:
+        return True
+    else:
+        return False
+
+
 def read_input():
-    seq = str(input())
+    seq = list(input())
     return seq
-
-
-def is_correct_bracket_seq(seq: str) -> bool:
-    print(len(seq))
-    # if seq[0] == '{' and seq[len(seq) - 1]  {[()]}
 
 
 if __name__ == '__main__':
