f3265e950988395db849feecc1f909ee88354763
Maidari Tsydenov <tmaidari@mail.ru>
2022-10-27 18:38:20 +0300

    b fixed

--- python/sprint13_final/B_fast_sort.py
+++ python/sprint13_final/B_fast_sort.py
@@ -1,43 +1,74 @@
-# 73064307
-def quicksort(array):
-    if len(array) <= 1:
-        return array
-    piv = array[len(array) // 2]
-    right = []
-    left = []
-    mid = []
-    for n in array:
-        if n[1] > piv[1]:
-            right.append(n)
-        elif n[1] < piv[1]:
-            left.append(n)
-        else:
-            if n[0] == piv[0]:
-                mid.append(n)
-            elif n[2] != piv[2]:
-                right.append(n) if n[2] < piv[2] else left.append(n)
-            else:
-                right.append(n) if n[0] < piv[0] else left.append(n)
-    return quicksort(right) + mid + quicksort(left)
+# 73182086
+import random
+
+
+class Person:
+    def __init__(self, name: str, points: int, fines: int) -> str:
+        self.name = name
+        self.points = points
+        self.fines = fines
+
+    def __repr__(self, name):
+        return name
+
+    def __gt__(self, other):
+        """ "Больше", т.е. ниже в списке.
+        Меньше решенных задач, больше штрафов и имя в алфавите ниже.
+        """
+        if self.points == other.points:
+            if self.fines == other.fines:
+                return self.name > other.name
+            return self.fines > other.fines
+        return self.points < other.points
+
+    def __lt__(self, other):
+        """ "Меньше", т.е. выше в списке.
+        Больше реешнных задач, меньше штрафов и имя в алфавите выше.
+        """
+        if self.points == other.points:
+            if self.fines == other.fines:
+                return self.name < other.name
+            return self.fines < other.fines
+        return self.points > other.points
+
+
+def quicksort(data, left, right):
+    # базовый случай
+    if left >= right:
+        return data
+    i, j = left, right
+    # случайный элемент из data
+    pivot = data[random.randint(left, right)]
+    # сортируем
+    while i <= j:
+        while data[i] < pivot:
+            i += 1
+        while data[j] > pivot:
+            j -= 1
+
+        if i <= j:
+            data[i], data[j] = data[j], data[i]
+            i, j = i + 1, j - 1
+
+    quicksort(data, left, j)
+    quicksort(data, i, right)
 
 
 def read_input():
     n = int(input())
-    array = []
+    data = []
     for i in range(n):
-        data = list(input().split())
-        data[1] = int(data[1])
-        data[2] = int(data[2])
-        array.append(data)
-    return array
+        name, points, fines = input().split()
+        data.append(Person(name, int(points), int(fines)))
+    return n, data
 
 
-def main(array):
-    sort = quicksort(array)
-    for s in sort:
-        print(s[0])
+def main(n, data):
+    quicksort(data, 0, n - 1)
+    for member in data:
+        print(member.name)
 
 
 if __name__ == '__main__':
-    array = read_input()
-    main(array)
+    n, data = read_input()
+    main(n, data)
